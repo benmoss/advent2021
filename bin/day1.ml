794594316cf739370befa107b31f44ca2a4c0604
Ben Moss <ben@mossity.com>
2021-12-01 20:20:55 -0500

    Update for challenge part 2

--- bin/day1.ml
+++ bin/day1.ml
@@ -1,23 +1,30 @@
 open Base
 open Stdio
 
+let windowed_prepend l x len =
+    let l = x :: l in
+    let reversed = List.rev l in
+    let to_drop = List.length l - len in
+    let dropped = List.drop reversed to_drop in
+    List.rev dropped
 
-let rec count_incrementing total previous =
+let rec count total acc =
     let line = In_channel.input_line In_channel.stdin in
     match line with
     | None -> total
-    | Some x ->
-        let current = Int.of_string x in
-        let total = if current > previous then total+1 else total in
-        count_incrementing total current
-
-let count =
-    let line = In_channel.input_line In_channel.stdin in
-    match line with
-    | None -> 0
-    | Some x ->
-        let current = Int.of_string x in
-        count_incrementing 0 current
+    | Some line ->
+        let int = Int.of_string line in
+        let current = windowed_prepend acc int 3 in
+        let total = if List.length acc = 3 then
+            let sum x = match List.reduce x ~f:(+) with
+            | None -> 0
+            | Some x -> x in
+            let previous_sum = sum acc in
+            let new_sum = sum current in
+            if new_sum > previous_sum then total+1 else total
+        else
+            total in
+        count total current
 
 let () =
-    printf "Total: %d\n" count
+    printf "Total: %d\n" (count 0 [])
